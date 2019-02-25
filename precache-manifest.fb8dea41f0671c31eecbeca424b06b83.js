0bbe176426d60504836d2a456520ecef566a23e1
r2dliu <dliu@smash.gg>
2019-02-24 21:13:23 -0800

    Updates

---
+++ precache-manifest.fb8dea41f0671c31eecbeca424b06b83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/r2dliu-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7e5c19c49915965e5450",
+    "url": "/r2dliu-website/static/js/runtime~main.7e5c19c4.js"
+  },
+  {
+    "revision": "5d2d3f6e7f47476278f2",
+    "url": "/r2dliu-website/static/js/main.5d2d3f6e.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/r2dliu-website/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d2d3f6e7f47476278f2",
+    "url": "/r2dliu-website/static/css/main.a76ac6b1.chunk.css"
+  },
+  {
+    "revision": "c209c25b17bce7f3bca9c4a27dede2bc",
+    "url": "/r2dliu-website/index.html"
+  }
+];
