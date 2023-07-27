1f6eb0393b075173a7833cc0dce3eedb316d2da8
R2DLiu <zliu3301@gmail.com>
2023-07-27 15:01:26 -0400

    global meta added to head

--- app/root.tsx
+++ app/root.tsx
@@ -35,11 +35,6 @@ export const meta: V2_MetaFunction = () => {
       property: "og:title",
       content: "R2DLiu.com",
     },
-    {
-      name: "viewport",
-      content: "width=device-width,initial-scale=1.0",
-    },
-    { charSet: "utf-8" },
   ];
 };
 
@@ -55,6 +50,8 @@ export function CatchBoundary() {
     <html>
       <head>
         <title>Oops!</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
