3075fe7ea032dccecbee52854f28a35b7010346f
David Liu <r2dliu@wireless-10-105-47-50.umd.edu>
2024-02-25 21:02:09 -0500

    shift stuff around. start building django/graphql backend

---
+++ api/r2dliu/courses/models.py
@@ -0,0 +1,21 @@
+# Create your models here.
+from django.db import models
+from r2dliu.utils.models import AbstractBaseModel
+
+
+class Course(AbstractBaseModel):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+
+class Class(AbstractBaseModel):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+
+class Subscriber(AbstractBaseModel):
+    name = models.CharField(max_length=200)
+    email = models.CharField(
+        unique=True,
+        max_length=200,
+    )
