70c69fe9076c41c90112265977bbb4a3da5f303c
David Liu <r2dliu@Davids-MacBook-Pro.local>
2024-10-06 02:44:30 -0400

    stacker

---
+++ web/app/routes/stacker/route.tsx
@@ -0,0 +1,14 @@
+// import { Button } from "@mui/material";
+// import { useNavigate } from "@remix-run/react";
+// import cn from "classnames";
+// import { useState } from "react";
+
+import styles from "./stacker.module.css";
+
+export default function Stacker() {
+  return (
+    <div className={styles.Home}>
+      Wowee
+    </div>
+  );
+}
