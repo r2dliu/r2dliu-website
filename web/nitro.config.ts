5dd078051d742d2fec81d788455eac1fee97c7c1
David Liu <zhiyuan.liu@axleinfo.com>
2025-11-10 15:24:18 -0500

    fix nitro dependency

--- web/nitro.config.ts
+++ web/nitro.config.ts
@@ -2,7 +2,6 @@ import { defineNitroConfig } from 'nitro/config'
 
 export default defineNitroConfig({
   serveStatic: true,
-  noExternals: false,
   rollupConfig: {
     external: ['react', 'react-dom', 'three', '@react-three/fiber', '@react-three/drei'],
   },
